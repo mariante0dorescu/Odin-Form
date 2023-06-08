233cae9bdc7946feef892f8f68f4755f5db70c83
mariante0dorescu <broinkus@gmail.com>
2023-06-08 17:10:31 +0300

    styles

---
+++ main.js
@@ -0,0 +1,9 @@
+// theme selector
+
+function setTheme(){
+  const root = document.documentElement;
+  const newTheme = root.className === "dark" ? "light" : "dark";
+  root.className = newTheme;
+}
+
+// document.querySelector('theme-toggle').addEventListener('click', setTheme)
