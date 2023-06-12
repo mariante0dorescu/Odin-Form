ba67a8db3e67a0333d0bac85e01533612a48a1fd
mariante0dorescu <broinkus@gmail.com>
2023-06-12 11:01:54 +0300

    theme switcher added

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // theme selector
-
-function setTheme(){
-  const root = document.documentElement;
-  const newTheme = root.className === "dark" ? "light" : "dark";
-  root.className = newTheme;
-}
-
-// document.querySelector('theme-toggle').addEventListener('click', setTheme)
+const input = document.querySelector(".theme-switcher input");
+input.addEventListener("change", (e) => {
+    if (e.target.checked) {
+        document.body.setAttribute("data-theme", "dark");
+    } else {
+        document.body.setAttribute("data-theme", "light");
+    }
+});
